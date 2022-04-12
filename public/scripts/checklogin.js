05543ff4505a6f489617b6c9bb526d31f634c44b
BinaryGreen <dannymango11@gmail.com>
2022-04-12 01:29:02 -0400

    checklogin now checks if user isn't logged in

--- public/scripts/checklogin.js
+++ public/scripts/checklogin.js
@@ -1,4 +1,4 @@
-// checks if user is logged in. if so, changes href to redirect to account page
+// checks if user is not logged in. if so, changes href to redirect to login page
 
 const link = document.getElementById('account');
 const accountButton = document.querySelector('.account');
@@ -7,7 +7,9 @@ const accountButton = document.querySelector('.account');
 if (accountButton) accountButton.addEventListener('click', checklogin);
 
 function checklogin() {
-    if (localStorage.getItem('user')) {
-        link.href = "account.html"
+    if (!localStorage.getItem('user')) {
+        link.href = "loginor.html";
+    } else {
+        link.href = "account.html";
     }
 }
